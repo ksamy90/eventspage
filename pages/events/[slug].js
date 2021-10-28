ae02c882e49e5d902c7be674a0840702921f2786
samson <ksamy90@aol.com>
2021-10-28 10:57:26 +0300

    initialize music events page

---
+++ pages/events/[slug].js
@@ -0,0 +1,9 @@
+import Layout from "../../components/Layout";
+
+export default function EventPage() {
+  return (
+    <Layout title="event page">
+      <h1>an event page</h1>
+    </Layout>
+  );
+}
